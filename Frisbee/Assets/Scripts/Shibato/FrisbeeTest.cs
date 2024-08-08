c60f8014ccdf98bc2aeda4bc08fc39d0db991a7b
sora <4810sora01@gmail.com>
2024-08-08 19:05:32 +0900

    アイテムキャッチと棘に触れたらフリスビーを消す

---
+++ Frisbee/Assets/Scripts/Shibato/FrisbeeTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shibato
+{
+    public class FrisbeeTest:MonoBehaviour
+    {
+        private Rigidbody rb;
+
+        private void Start()
+        {
+            rb = transform.GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            Vector3 force = new Vector3(1, 0, 0);
+            rb.AddForce(force, ForceMode.Force);
+            
+        }
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (collision.gameObject.CompareTag("Item"))
+            {
+                GameObject copiedObject =
+                    Instantiate(collision.gameObject, transform.position + Vector3.up, Quaternion.identity,this.transform);
+        
+                // コピーしたオブジェクトの見た目を同じにする
+                copiedObject.transform.localScale = collision.transform.localScale;
+                
+                Destroy(collision.gameObject);
+            }
+
+            if (collision.gameObject.CompareTag("spines"))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
