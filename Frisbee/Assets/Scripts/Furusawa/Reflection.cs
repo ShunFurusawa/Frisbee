96ee0b57516035796b219f844435b3bbb972e885
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2024-11-13 15:22:14 +0900

    微調整

--- Frisbee/Assets/Scripts/Furusawa/Reflection.cs
+++ Frisbee/Assets/Scripts/Furusawa/Reflection.cs
@@ -18,6 +18,7 @@ namespace Furusawa
             {
                 _rb = other.gameObject.GetComponent<Rigidbody>();
             }
+
             
             // 入射ベクトル (速度）
             Vector3 inDirection = _rb.velocity;
@@ -26,13 +27,14 @@ namespace Furusawa
             Debug.Log("normal = " + other.contacts[0].normal);
            //Vector3 inNormal =  Vector3.up;
             // 反射ベクトル（速度）
-            inNormal = Vector3.Scale(inNormal, new Vector3(-1f, 1f, 1f));
+            inNormal = Vector3.Scale(inNormal, new Vector3(1f, 1f, 1f));
             Vector3 result = Vector3.Reflect(inDirection, inNormal);
-          
+        
             // 反射後の速度を反映
             _rb.velocity = Vector3.Scale(result, scalePower);
             StartCoroutine("WaitAndReflective");
             Debug.Log("reflection!");
+         
         }
 
         //OnCollisionが何度も起きないように待機時間入れてみる
