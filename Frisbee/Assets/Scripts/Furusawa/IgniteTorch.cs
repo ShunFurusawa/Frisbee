0ecef0c85d3b49b529c42dcb05a5119f525a086f
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2024-11-14 12:03:13 +0900

    ライト点灯するように

---
+++ Frisbee/Assets/Scripts/Furusawa/IgniteTorch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Furusawa
+{
+    public class IgniteTorch : MonoBehaviour
+    {
+        private Light _lightComp;
+        
+        [Header("松明は〇fの明るさで点灯する")]
+        [SerializeField] private float brightness = default!;
+        private void Start()
+        {
+            _lightComp = GetComponent<Light>();
+            _lightComp.intensity = 0f;
+        }
+
+        public void Ignite()
+        {
+            if (_lightComp == null)
+            {
+                _lightComp = GetComponent<Light>();
+                Debug.LogAssertion("light component is null!");
+            }
+
+            _lightComp.intensity = brightness;
+        }
+    }
+}
--- Frisbee/Assets/Scripts/Furusawa/RespawnArea.cs
+++ Frisbee/Assets/Scripts/Furusawa/RespawnArea.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Furusawa
 {
     public class RespawnArea : MonoBehaviour
     {
         [SerializeField] private Transform respawnPoint;
+       
+        [Header("松明")]
+        [SerializeField] private IgniteTorch[] Torches;
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
                 if (GameManager.instance.SavePoint != respawnPoint.position)
-                  GameManager.instance.SavePoint = respawnPoint.position;
+                {
+                    GameManager.instance.SavePoint = respawnPoint.position;
+                    SetTorches();
+                }
+            }
+        }
+
+        private void SetTorches()
+        {
+            foreach (IgniteTorch torch in Torches)
+            {
+                torch.Ignite();
             }
         }
     }
