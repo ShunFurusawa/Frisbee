d8a27b0dd797dbb6006a0644ccce819e62aaaba6
sora <4810sora01@gmail.com>
2025-01-09 04:48:50 +0900

    スイッチ関連

--- Frisbee/Assets/Scripts/Shibato/Switch.cs
+++ Frisbee/Assets/Scripts/Shibato/Switch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,24 @@ public class Switch : MonoBehaviour
     
     [SerializeField, JapaneseLabel("消すオブジェクト")]
     private GameObject[] deadGameObjects;
+    [JapaneseLabel("ONのマテリアル")][SerializeField] private Material ON;
+    [JapaneseLabel("OFFのマテリアル")][SerializeField] private Material OFF;
+    private Renderer windRenderer;
+
+    private void Awake()
+    {
+        windRenderer = GetComponent<Renderer>();
+        windRenderer.material = OFF;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        foreach (GameObject obj in deadGameObjects) // 配列内の全要素を処理
+        foreach (var obj in deadGameObjects) // 配列内の全要素を処理
         {
             if (obj != null) // null チェック
             {
                 obj.SetActive(false); // 非アクティブ化
+                windRenderer.material = ON;
             }
         }
     }
